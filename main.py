20ebffdb5b7372477f861b6ef4822071d9b94602
Weverton Vitor <wevertonvitor01@gmail.com>
2024-04-14 20:25:08 -0300

    Add Gausian Mixed Model

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from PIL import Image
 import numpy as np
 from sklearn.cluster import KMeans
+import os
+from sklearn.mixture import GaussianMixture
 
-def kmeans_segmentation(image_path, num_clusters, output_path):
-    # Abre a imagem
+
+
+def kmeans_segmentation(image_path, num_clusters, output_path, convert_to_gray=False):
+    # Abrir a imagem
     image = Image.open(image_path)
+    
+    # Converter para tons de cinza, se necessário
+    if convert_to_gray:
+        image = image.convert('L')
+
     # Converte a imagem para um array numpy
     image_array = np.array(image)
+
     # Obtém a forma da imagem
-    rows, cols, _ = image_array.shape
+    rows, cols = 0, 0
+    num_channels = 1
+    if len(image_array.shape) > 2:
+        rows, cols, _ = image_array.shape
+        num_channels = image_array.shape[2]
+    else:
+        rows, cols = image_array.shape
+
+
     # Redimensiona o array para que cada pixel seja uma amostra
-    image_array_flat = image_array.reshape((-1, 3))
+    image_array_flat = image_array.reshape((-1, num_channels))
     
     # Aplica o algoritmo K-Means
     kmeans = KMeans(n_clusters=num_clusters, random_state=42)
@@ -23,8 +41,14 @@ def kmeans_segmentation(image_path, num_clusters, output_path):
     
     # Atualiza os valores dos pixels na imagem original com base nos centróides dos clusters
     segmented_image_array_flat = centroids[labels]
+
     # Redimensiona a imagem segmentada de volta para a forma original
-    segmented_image_array = segmented_image_array_flat.reshape((rows, cols, 3))
+    segmented_image_array = []
+    if convert_to_gray:
+        segmented_image_array = segmented_image_array_flat.reshape((rows, cols))
+    else:
+        segmented_image_array = segmented_image_array_flat.reshape((rows, cols, num_channels))
+
     # Converte o array numpy de volta para uma imagem PIL
     segmented_image = Image.fromarray(segmented_image_array.astype('uint8'))
 
@@ -32,16 +56,73 @@ def kmeans_segmentation(image_path, num_clusters, output_path):
     segmented_image.save(output_path)
 
 
-import os
+
+def gmm_segmentation(image_path, num_clusters, output_path, convert_to_gray=False):
+    # Abrir a imagem
+    image = Image.open(image_path)
+    
+    # Converter para tons de cinza, se necessário
+    if convert_to_gray:
+        image = image.convert('L')
+
+    # Converte a imagem para um array numpy
+    image_array = np.array(image)
+
+    # Obtém a forma da imagem
+    rows, cols = 0, 0
+    num_channels = 1
+    if len(image_array.shape) > 2:
+        rows, cols, _ = image_array.shape
+        num_channels = image_array.shape[2]
+    else:
+        rows, cols = image_array.shape
+    # Redimensiona o array para que cada pixel seja uma amostra
+    image_array_flat = image_array.reshape((-1, num_channels))
+    
+    # Aplica o algoritmo GMM
+    gmm = GaussianMixture(n_components=num_clusters, random_state=42)
+    gmm.fit(image_array_flat)
+    
+    # Obtém os rótulos dos clusters para cada pixel
+    labels = gmm.predict(image_array_flat)
+    # Obtém as médias dos componentes para os clusters
+    means = gmm.means_
+    
+    # Atualiza os valores dos pixels na imagem original com base nas médias dos componentes dos clusters
+    segmented_image_array_flat = means[labels]
+
+    # Redimensiona a imagem segmentada de volta para a forma original
+    segmented_image_array = []
+    if convert_to_gray:
+        segmented_image_array = segmented_image_array_flat.reshape((rows, cols))
+    else:
+        segmented_image_array = segmented_image_array_flat.reshape((rows, cols, num_channels))
+
+
+    # Converte o array numpy de volta para uma imagem PIL
+    segmented_image = Image.fromarray(segmented_image_array.astype('uint8'))
+    
+
+    # Salva a imagem segmentada
+    segmented_image.save(output_path)
 
 images_dir = './images'
 masks_dir = './masks'
 
 images_path = os.listdir(images_dir)
-print(images_path)
 
 # Exemplo de uso:
 for image_path in images_path:
-    output_path = f"{masks_dir}/{image_path.split('.')[0]}_mask.{image_path.split('.')[1]}"
-    num_clusters = 10  # Número de clusters para o algoritmo K-Means
-    kmeans_segmentation(f'{images_dir}/{image_path}', num_clusters, output_path)
+    output_path = f"{masks_dir}/{image_path.split('.')[0]}_kmeans_mask.{image_path.split('.')[1]}"
+    output_path_gray = f"{masks_dir}/{image_path.split('.')[0]}_kmeans_mask_gray.{image_path.split('.')[1]}"
+
+    num_clusters = 6  # Número de clusters para o algoritmo K-Means
+
+    kmeans_segmentation(f'{images_dir}/{image_path}', num_clusters, output_path, convert_to_gray=False)
+    kmeans_segmentation(f'{images_dir}/{image_path}', num_clusters, output_path_gray, convert_to_gray=True)
+    
+    output_path = f"{masks_dir}/{image_path.split('.')[0]}_gmm_mask.{image_path.split('.')[1]}"
+    output_path_gray = f"{masks_dir}/{image_path.split('.')[0]}_gmm_mask_gray.{image_path.split('.')[1]}"
+
+    gmm_segmentation(f'{images_dir}/{image_path}', num_clusters, output_path, convert_to_gray=False)
+    gmm_segmentation(f'{images_dir}/{image_path}', num_clusters, output_path_gray, convert_to_gray=True)
---
+++ main_backup.py
@@ -0,0 +1,80 @@
+from PIL import Image
+import numpy as np
+from sklearn.cluster import KMeans
+
+def kmeans_segmentation(image_path, num_clusters, output_path):
+    # Abre a imagem
+    image = Image.open(image_path)
+    # Converte a imagem para um array numpy
+    image_array = np.array(image)
+    # Obtém a forma da imagem
+    rows, cols, _ = image_array.shape
+    # Redimensiona o array para que cada pixel seja uma amostra
+    image_array_flat = image_array.reshape((-1, 3))
+    
+    # Aplica o algoritmo K-Means
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    kmeans.fit(image_array_flat)
+    
+    # Obtém os rótulos dos clusters para cada pixel
+    labels = kmeans.labels_
+    # Obtém os centróides dos clusters
+    centroids = kmeans.cluster_centers_
+    
+    # Atualiza os valores dos pixels na imagem original com base nos centróides dos clusters
+    segmented_image_array_flat = centroids[labels]
+    # Redimensiona a imagem segmentada de volta para a forma original
+    segmented_image_array = segmented_image_array_flat.reshape((rows, cols, 3))
+    # Converte o array numpy de volta para uma imagem PIL
+    segmented_image = Image.fromarray(segmented_image_array.astype('uint8'))
+
+    # Salva a imagem segmentada
+    segmented_image.save(output_path)
+
+def kmeans_segmentation_gray_scale(image_path, num_clusters, output_path):
+    # Abre a imagem
+    image = Image.open(image_path)
+    # Converte a imagem para escala de cinza
+    image_gray = image.convert('L')
+    # Converte a imagem para um array numpy
+    image_array = np.array(image_gray)
+    # Obtém a forma da imagem
+    rows, cols = image_array.shape
+    # Redimensiona o array para que cada pixel seja uma amostra
+    image_array_flat = image_array.reshape((-1, 1))  # Apenas uma dimensão para tons de cinza
+    
+    # Aplica o algoritmo K-Means
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    kmeans.fit(image_array_flat)
+    
+    # Obtém os rótulos dos clusters para cada pixel
+    labels = kmeans.labels_
+    # Obtém os centróides dos clusters
+    centroids = kmeans.cluster_centers_
+    
+    # Atualiza os valores dos pixels na imagem original com base nos centróides dos clusters
+    segmented_image_array_flat = centroids[labels]
+    # Redimensiona a imagem segmentada de volta para a forma original
+    segmented_image_array = segmented_image_array_flat.reshape((rows, cols))
+    # Converte o array numpy de volta para uma imagem PIL
+    segmented_image = Image.fromarray(segmented_image_array.astype('uint8'))
+
+    # Salva a imagem segmentada
+    segmented_image.save(output_path)
+
+
+import os
+
+images_dir = './images'
+masks_dir = './masks'
+
+images_path = os.listdir(images_dir)
+print(images_path)
+
+# Exemplo de uso:
+for image_path in images_path:
+    output_path = f"{masks_dir}/{image_path.split('.')[0]}_mask.{image_path.split('.')[1]}"
+    output_path_gray = f"{masks_dir}/{image_path.split('.')[0]}_mask_gray.{image_path.split('.')[1]}"
+    num_clusters = 6  # Número de clusters para o algoritmo K-Means
+    kmeans_segmentation(f'{images_dir}/{image_path}', num_clusters, output_path)
+    kmeans_segmentation_gray_scale(f'{images_dir}/{image_path}', num_clusters, output_path_gray)
